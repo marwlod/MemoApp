bd696a6b230aa05922fa7df3a98c8fd576b329c3
mardzin <martin.wlod@gmail.com>
2018-12-12 21:50:55 +0100

    Added lang choice support over default browser lang, buttons, comments

---
+++ src/main/java/io/github/marwlod/memoapp/config/InternationalizationConfig.java
@@ -0,0 +1,31 @@
+package io.github.marwlod.memoapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class InternationalizationConfig implements WebMvcConfigurer {
+
+    // will switch locale based on the URL parameter value
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
